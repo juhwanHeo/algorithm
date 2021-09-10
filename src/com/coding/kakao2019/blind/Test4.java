1844fda78ea3a67822cdde02d31b9accc115013b
juhwanHeo <juhwanheo.dev@gmail.com>
2021-09-10 19:02:00 +0900

    21.09.10 - 2
    1, level3
      - 해결
        - 네트워크
        - 여행 경로
    
    2, kakao2019 blind
      - 해결
        - 무지의 먹방 라이브
        - 길 찾기 게임
    
    3, kakao2020 blind
      - 해결
        - 괄호 변환

--- src/com/coding/kakao2019/blind/Test4.java
+++ src/com/coding/kakao2019/blind/Test4.java
@@ -4,130 +4,69 @@ import java.util.*;
 
 /*
  * @2019 kakao blind recruitment
- * @TestName: 길 찾기 게임
- * @URL: https://programmers.co.kr/learn/courses/30/lessons/42892
+ * @TestName: 무지의 먹방 라이브
+ * @URL: https://programmers.co.kr/learn/courses/30/lessons/42891
  */
 public class Test4 {
 
-    static class Node {
-        Point data;
-        Node root;
-        Node left;
-        Node right;
+    static class Food implements Comparable<Food> {
+        int index;
+        int time;
+
+        public Food(int index, int time) {
+            this.index = index;
+            this.time = time;
+        }
 
         @Override
-        public String toString() {
-            return data + " ";
-        }
-    }
-    static class Point {
-        int val;
-        int x;
-        int y;
-
-        Point(int x, int y, int val) {
-            this.x = x;
-            this.y = y;
-            this.val = val;
+        public int compareTo(Food food) {
+            return Integer.compare(time, food.time);
         }
 
         @Override
         public String toString() {
-            return "Point{" +
-                    "val=" + val +
-                    ", x=" + x +
-                    ", y=" + y +
+            return "Food{" +
+                    "index=" + index +
+                    ", time=" + time +
                     '}';
         }
     }
 
-    public static int[][] solution(int[][] nodeinfo) {
-        List<Point> list = new ArrayList<>();
-        List<Integer> preorderList = new ArrayList<>();
-        List<Integer> postorderList = new ArrayList<>();
-        Node root = null;
-        for (int i = 0; i < nodeinfo.length; i++) {
-            list.add(new Point(nodeinfo[i][0], nodeinfo[i][1], i+1));
+
+    public static int solution(int[] food_times, long k) {
+//        if(Arrays.stream(food_times).mapToLong(Long::valueOf).sum() <= k) return -1;
+        List<Food> list = new ArrayList<>();
+        long sum = 0;
+        for (int i = 0; i < food_times.length; i++) {
+            list.add(new Food(i + 1, food_times[i]));
+            sum += food_times[i];
         }
-        list.sort((p1, p2) -> p2.y - p1.y);
+        if (sum <= k) return -1;
+        Collections.sort(list);
 
-        for (Point point : list) {
-            Node newNode = new Node();
-            newNode.data = point;
-            if (root == null) {
-                root = newNode;
-            }
-            else {
-                // 탐색
-                Node curNode = root;
-                while(true) {
-                    // left
-                    if (curNode.data.x > newNode.data.x) {
-                        if (curNode.left == null) {
-                            curNode.left = newNode;
-                            newNode.root = curNode;
-                            break;
-                        }
-                        else curNode = curNode.left;
-                    }
-                    // right
-                    else {
-                        if(curNode.right == null) {
-                            curNode.right = newNode;
-                            newNode.root = curNode;
-                            break;
-                        }
-                        else curNode = curNode.right;
-                    }
+        long time = 0; // 먹은 시간
+        long previous = 0; // 전에 먹은 음식 시간
+        long length = food_times.length; // 남은 음식 개수
 
-                }
-
-            }
+        while(time + ((list.get(0).time - previous) * length) <= k) {
+            int now = list.remove(0).time;
+            time += (now - previous) * length;
+            length--;
+            previous = now;
         }
 
-        preorder(root, preorderList);
-        postorder(root, postorderList);
-        return new int[][]{
-                preorderList.stream().mapToInt(Integer::valueOf).toArray(),
-                postorderList.stream().mapToInt(Integer::valueOf).toArray(),
-        };
-    }
-
-    // D L R
-    private static void preorder(Node node, List<Integer> list) {
-        if (node != null) {
-            list.add(node.data.val);
-            preorder(node.left, list);
-            preorder(node.right, list);
-        }
-    }
-
-    // L R D
-    private static void postorder(Node node, List<Integer> list) {
-        if(node != null) {
-            postorder(node.left, list);
-            postorder(node.right, list);
-            list.add(node.data.val);
-        }
-    }
-
-    // L D R
-    // 그냥 구현
-    private static void inorder(Node node) {
-        if (node != null) {
-            inorder(node.left);
-            System.out.println(node.data.val);
-            inorder(node.right);
-        }
+        list.sort(Comparator.comparingInt(a -> a.index));
+        return list.get((int) (k % length)).index;
     }
 
     public static void main(String[] args) {
-        int[][] relation1 = {{5,3},{11,5},{13,3},{3,5},{6,1},{1,3},{8,6},{7,2},{2,2}};
-        int[][] res1 = {{7,4,6,9,1,8,5,2,3},{9,6,5,8,1,4,3,2,7}};
-        int[][] myRes1 = solution(relation1);
+        int[] food_times = {
+                3, 1, 2
+        };
+        int k = 5;
+        int res = 1;
 
-        System.out.println("myRes1: "+Arrays.deepToString(myRes1));
-        System.out.println(Arrays.deepEquals(res1, myRes1));
+        System.out.println(solution(food_times, k));
 
     }
 }
--- src/com/coding/level3/Network.java
+++ src/com/coding/level3/Network.java
@@ -1,24 +1,60 @@
 package com.coding.level3;
 
-import java.util.Arrays;
+import java.util.*;
 /*
  * @level3
  * @TestName: 네트워크
  * @URL: https://programmers.co.kr/learn/courses/30/lessons/43162
  */
 public class Network {
-
+    static List<List<Integer>> list;
+    static boolean[] visited;
     public static int solution(int n, int[][] computers) {
-        int one = 0;
-        for (int[] computer : computers) {
-            one += findOne(computer);
-        }
-        if(n == one) return n;
-        System.out.println("one: " + one);
-        System.out.println("n: " + n);
-        return (int) (n - Math.round((one - n) / 2.0));
+        int answer = 0;
+        visited = new boolean[n];
+        list = new ArrayList<>();
 
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n; k++) {
+                if (i == k) continue;
+                if (computers[i][k] == 1) list.get(i).add(k);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]){
+                dfs(i);
+                answer++;
+            }
+        }
+
+        return answer;
     }
+
+    private static void dfs(int node) {
+        for (Integer n : list.get(node)) {
+            if (!visited[n]) {
+                visited[n] = true;
+                dfs(n);
+            }
+        }
+    }
+
+//    public static int solution(int n, int[][] computers) {
+//        int one = 0;
+//        for (int[] computer : computers) {
+//            one += findOne(computer);
+//        }
+//        if(n == one) return n;
+//        System.out.println("one: " + one);
+//        System.out.println("n: " + n);
+//        return (int) (n - Math.round((one - n) / 2.0));
+//
+//    }
 
     static long findZero(int[] computers) {
         return Arrays.stream(computers)
