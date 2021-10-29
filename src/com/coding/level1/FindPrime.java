bba07be02df5fec3adf4bfd757e4dbffcd0239e1
juhwanHeo <juhwanheo.dev@gmail.com>
2021-10-29 14:55:20 +0900

    21.10.29 - 1 (mac)
    1, level1
      - 해결
        - 약수의 합
        - 소수 찾기
        - 문자열 내 마음대로 정렬하기
        - 문자열 내 p와 y의 개수
        - 문자열 내림차순으로 배치하기
        - 수박수박수박수박수박수?

---
+++ src/com/coding/level1/FindPrime.java
@@ -0,0 +1,34 @@
+package com.coding.level1;
+
+/*
+ * @level1
+ * @TestName: 소수 찾기
+ * @URL: https://programmers.co.kr/learn/courses/30/lessons/12921
+ */
+public class FindPrime {
+    static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) answer++;
+        }
+
+        return answer;
+    }
+
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+//        for (int i = 2; i <= Math.sqrt(n); i++) {
+//            if (n % i == 0) return false;
+//        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("solution(10) = " + solution(10)); // 4
+    }
+}
