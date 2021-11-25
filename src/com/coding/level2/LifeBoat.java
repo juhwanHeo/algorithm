1860aa603cf64c58eef3b4adb03494a53d23e4db
juhwanHeo <juhwanheo.dev@gmail.com>
2021-11-25 14:23:43 +0900

    21.11.25 - 1 (mac)
    1, level2
      - 해결
        - 다음 큰 숫자
        - 구명보트

---
+++ src/com/coding/level2/LifeBoat.java
@@ -0,0 +1,37 @@
+package com.coding.level2;
+
+import java.util.*;
+
+/*
+ * @level2
+ * @TestName: 구명보트
+ * @URL: https://programmers.co.kr/learn/courses/30/lessons/42885
+ */
+public class LifeBoat {
+
+    public static int solution(int[] people, int limit) {
+        int answer = 0;
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        Arrays.sort(people);
+        for (int person : people) deque.add(person);
+
+        while (!deque.isEmpty()) {
+            int removeVal = deque.removeLast();
+            if (!deque.isEmpty() && limit >= removeVal + deque.getFirst())
+                deque.removeFirst();
+            answer++;
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        int[] people = {
+                70, 50, 80, 50
+        };
+
+        System.out.println(solution(people, 100));
+    }
+}
