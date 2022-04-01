fb0b55d3647798c24bdbd7bdf2c836c5f8c3b804
juhwanHeo <juhwanheo.dev@gmail.com>
2022-04-01 17:09:58 +0900

    22.04.01 - 1 (mac)
    # DO
    1, level2
      - N개의 최소공배수
    
    2, level3
      - 단어변환

---
+++ src/com/coding/level2/MinCommonN.java
@@ -0,0 +1,33 @@
+package com.coding.level2;
+
+/*
+ * @level2
+ * @TestName: N개의 최소공배수
+ * @URL: https://programmers.co.kr/learn/courses/30/lessons/12953
+ */
+public class MinCommonN {
+    public static int solution(int[] arr) {
+        if (arr.length == 1) return arr[0];
+
+        int lcm = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int gcd = gcd(lcm, arr[i]);
+            lcm = (lcm * arr[i]) / gcd;
+        }
+
+        return lcm;
+    }
+
+    private static int gcd(int big, int small) {
+        return (big % small == 0) ? small : gcd(small, big % small);
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = {
+                2, 6, 8, 14
+        };
+
+        System.out.println("solution(arr) = " + solution(arr));
+    }
+}
