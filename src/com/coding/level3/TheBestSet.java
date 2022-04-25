226ef2daa3842b439fd24b1a4e73433899755eee
juhwanHeo <juhwanheo.dev@gmail.com>
2022-04-25 10:56:31 +0900

    22.04.25 - 1 (mac)
    # DO
    1, 조합 Utils 추가
    
    2, kakao2020 blind
       - 외벽 점검
    
    3, kakao2022 blind
       - 양과 늑대
       - 파괴되지 않는 건물
    
    4, level 3
       - 최고의 집합

---
+++ src/com/coding/level3/TheBestSet.java
@@ -0,0 +1,39 @@
+package com.coding.level3;
+
+import java.util.*;
+
+/*
+ * @level3
+ * @TestName: 최고의 집합
+ * @URL: https://programmers.co.kr/learn/courses/30/lessons/12938?language=java
+ */
+public class TheBestSet {
+    public static int[] solution(int n, int s) {
+        if (s < n) return new int[] {-1};
+        int[] answer = new int[n];
+
+//        answer = Arrays.stream(answer)
+//                .map(val -> val = s / n)
+//                .toArray();
+
+        for (int i = 0; i < n; i++) answer[i] = s / n;
+
+        for (int i = 0; i < s % n; i++) answer[i]++;
+
+        Arrays.sort(answer);
+        return answer;
+
+//        return Arrays.stream(answer).sorted().toArray();
+    }
+
+
+    public static void main(String[] args) {
+        int n1 = 2;
+        int s = 9;
+//        int s = 1;
+
+        System.out.println(Arrays.toString(solution(n1, s)));
+
+
+    }
+}
