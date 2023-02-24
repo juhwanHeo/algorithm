918cd85675cfb6898d92a5a5691eef0613db8f5f
juhwanHeo <juhwanheo.dev@gmail.com>
2023-02-25 02:42:05 +0900

    23.02.25 - 1 (desktop)

--- src/main/java/com/programmers/kakao2023/blind/Test1.java
+++ src/main/java/com/programmers/kakao2023/blind/Test1.java
@@ -1,15 +1,42 @@
 package com.programmers.kakao2023.blind;
 
+import java.util.*;
+
 public class Test1 {
-    public static int solution() {
-        int answer = 0;
-        return answer;
+
+    public static int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        int todayValue = getMonth(today);
+        for (String term : terms) {
+            String[] split = term.split(" ");
+            map.put(split[0], Integer.parseInt(split[1]));
+        }
+
+        int index = 1;
+        for (String privacy : privacies) {
+            String[] split = privacy.split(" ");
+            String date = split[0];
+            String type = split[1];
+            int termDay = getMonth(date);
+            int diff = (todayValue - termDay) / 28;
+            if (map.get(type) <= diff) list.add(index);
+
+            index++;
+        }
+
+        return list.stream()
+                .sorted()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 
-    private static void test(int a) {
-        if (a == 1) return;
-        // rkekfksaklfnklwnalknfdafndsarrefedsafndjlksarfdsavvcxzfdsar
-
+    private static int getMonth(String date) {
+        String[] split = date.split("\\.");
+        int year = Integer.parseInt(split[0]) * 12 * 28;
+        int month = Integer.parseInt(split[1]) * 28;
+        int day = Integer.parseInt(split[2]);
+        return year + month + day;
     }
-
 }
--- src/test/java/com/programmers/kakao2023/blind/Test1Test.java
+++ src/test/java/com/programmers/kakao2023/blind/Test1Test.java
@@ -2,16 +2,44 @@ package com.programmers.kakao2023.blind;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Test1Test {
 
     @Test
     public void solution1() {
-        int n = 6;
-        int expected = 0;
-        int result = Test1.solution();
-        System.out.println("result = " + result);
-        assertEquals(expected, result);
+        String today = "2022.05.19";
+        String[] terms = {
+                "A 6", "B 12", "C 3"
+        };
+        String[] privacies = {
+                "2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"
+        };
+
+
+        int[] expected = {1, 3};
+        int[] result = Test1.solution(today, terms, privacies);
+        System.out.println("result = " + Arrays.toString(result));
+        assertArrayEquals(expected, result);
+    }
+
+
+    @Test
+    public void solution2() {
+        String today = "2020.01.01";
+        String[] terms = {
+                "Z 3", "D 5"
+        };
+        String[] privacies = {
+                "2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"
+        };
+
+
+        int[] expected = {1, 4, 5};
+        int[] result = Test1.solution(today, terms, privacies);
+        System.out.println("result = " + Arrays.toString(result));
+        assertArrayEquals(expected, result);
     }
 }
