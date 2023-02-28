78908db6bca047a410b14de5b4888808ee1ac17f
juhwanHeo <juhwanheo.dev@gmail.com>
2023-03-01 00:43:34 +0900

    23.03.01 - 2 (desktop)
    ## 주석 추가

--- src/main/java/com/programmers/kakao2023/blind/Test1.java
+++ src/main/java/com/programmers/kakao2023/blind/Test1.java
@@ -2,6 +2,11 @@ package com.programmers.kakao2023.blind;
 
 import java.util.*;
 
+/*
+ * @2023 KAKAO BLIND RECRUITMENT
+ * @TestName: 개인정보 수집 유효기간
+ * @URL: https://programmers.co.kr/learn/courses/30/lessons/150370
+ */
 public class Test1 {
 
     public static int[] solution(String today, String[] terms, String[] privacies) {
